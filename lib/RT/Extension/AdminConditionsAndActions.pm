f6721c24592206955f8b4ac9f38335c5c280d70f
Alex Vandiver <alexmv@bestpractical.com>
2014-12-18 14:38:07 -0500

    Adjust wording to include standard 4.2 Plugin() wording

--- lib/RT/Extension/AdminConditionsAndActions.pm
+++ lib/RT/Extension/AdminConditionsAndActions.pm
@@ -90,7 +90,11 @@ May need root permissions
 
 =item Edit your F</opt/rt4/etc/RT_SiteConfig.pm>
 
-Add this line:
+If you are using RT 4.2 or greater, add this line:
+
+    Plugin('RT::Extension::AdminConditionsAndActions');
+
+For RT 4.0, add this line:
 
     Set(@Plugins, qw(RT::Extension::AdminConditionsAndActions));
 
